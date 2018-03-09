1d1c8522b21f2f1bfc2dd135bd577804ed27e469
Felipe Guerra <fguerra0310@gmail.com>
2018-03-09 19:12:43 -0300

    Gulp - Minificar imagens

--- Gulp/gulpfile.js
+++ Gulp/gulpfile.js
@@ -1,20 +1,29 @@
 var gulp = require('gulp'),
   sass = require('gulp-sass'),
   concat = require('gulp-concat'),
-  uglify = require('gulp-uglify');
+  uglify = require('gulp-uglify'),
+  imagemin = require('gulp-imagemin');
 
-gulp.task('default', ['sass','js']);
+gulp.task('default', ['sass', 'js', 'image']);
 
 gulp.task('sass', function() {
   return gulp.src('assets/src/sass/**/*.scss')
     .pipe(concat('style.min.css'))
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
+    .pipe(sass({
+      outputStyle: 'compressed'
+    }).on('error', sass.logError))
     .pipe(gulp.dest('assets/css'));
 });
 
-gulp.task('js', function(){
+gulp.task('js', function() {
   return gulp.src('assets/src/js/**/*.js')
     .pipe(concat('script.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('assets/js'));
-})
+});
+
+gulp.task('image', function() {
+  return gulp.src('assets/src/img/*')
+    .pipe(imagemin())
+    .pipe(gulp.dest('assets/img'))
+});
